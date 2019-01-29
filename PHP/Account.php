3aee4a55e4ad30cb83cb6d29c0d458f22ca9defb
Ing. Alberto González Balderas <albertoglezb@hotmail.com>
2019-01-29 07:15:35 -0600

    Se crean los constructores

--- PHP/Account.php
+++ PHP/Account.php
@@ -3,6 +3,13 @@
 class Account{
     public $id;
     public $document;
+    public $name;
     public $email;
     public $password;
+
+    public function __construct($pName,$pDocument){
+       $this->name= $pName;
+       $this->document= $pDocument;
+    }
+
 }
--- PHP/Car.php
+++ PHP/Car.php
@@ -4,4 +4,14 @@ class Car{
     public $license;
     public $driver;
     public $passegenger;
+
+    public function __construct($pLicense, $pDriver){
+        $this->license = $pLicense;
+        $this->driver=$pDriver;
+    }
+
+    public function printDataCarAsString(){
+        return "License: " . $license . " Name driver: " . $driver;
+    }
+
 }
